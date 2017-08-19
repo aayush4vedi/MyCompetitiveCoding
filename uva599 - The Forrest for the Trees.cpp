a76e26ff2b2fc1457dad0832fb120db942bb91b7
@4vedi <4vedi.aayush@gmail.com>
2017-08-19 23:09:02 +0530

    Add files via upload

---
+++ uva599 - The Forrest for the Trees.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+#define nl endl
+#define ll long long
+#define vb vector<bool>
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<int> > vvc;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<int, string> mis;
+typedef map<string, bool> msb;
+typedef map<string, string> mss;
+typedef map<string, char> msc;
+typedef pair<int, int> ii; 
+typedef vector<ii> vii;
+#define FOR(i,a,b) for(int i=a; i<b;i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define IterV (j, v) for(vector<int>::iterator j = v.begin(); j!=v.end();j++)
+#define pb push_back
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define all(ar) ar.begin(), ar.end()
+#define pq priority_queue
+inline LL Power(int b, int p) { LL ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+
+
+vector<int>::iterator j;
+
+
+using namespace std;
+ 
+vector <int> adj[10];
+    bool visited[10];
+
+    void dfs(int s) {
+        visited[s] = true;
+        for(int i = 0;i < adj[s].size();++i)    {
+         if(visited[adj[s][i]] == false)
+             dfs(adj[s][i]);
+        }
+    }
+
+    void initialize() {
+        for(int i = 0;i < 10;++i)
+         visited[i] = false;
+    }
+
+    int main() {
+        int nodes, edges, x, y, connectedComponents = 0, fortest=0,acorns=0;
+        cin >> nodes;                       //Number of nodes
+        cin >> edges;                       //Number of edges
+        for(int i = 0;i < edges;++i) {
+         cin >> x >> y;     
+     //Undirected Graph 
+         adj[x].push_back(y);                   //Edge from vertex x to vertex y
+         adj[y].push_back(x);                   //Edge from vertex y to vertex x
+        }
+
+        initialize();                           //Initialize all nodes as not visited
+
+        for(int i = 1;i <= nodes;++i) {
+         if(visited[i] == false)     {
+             dfs(i);
+             connectedComponents++;
+         }
+         if(connectedComponents >1){
+         	fortest++;
+         }else{
+         	acorns++;
+         }
+        }
+        cout << "There are "<<fortest<<"(s) and "<<acorns<<" acorns(s)."<<'\n';
+        return 0;
+    }
+
