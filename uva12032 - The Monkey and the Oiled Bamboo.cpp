2efce28183437aaaf38b76c26d0cf0eecf142968
@4vedi <4vedi.aayush@gmail.com>
2017-08-20 23:50:05 +0530

    Add files via upload

---
+++ uva12032 - The Monkey and the Oiled Bamboo.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+#define nl endl
+#define ll long long
+#define vb vector<bool>
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<int, string> mis;
+typedef map<string, bool> msb;
+typedef map<string, string> mss;
+typedef map<string, char> msc;
+typedef pair<int, int> pii; 
+typedef vector<pii> vii;
+#define FOR(i,a,b) for(int i=a; i<b;i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define IterV (j, v) for(vector<int>::iterator j = v.begin(); j!=v.end();j++)
+#define pb push_back
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define all(ar) ar.begin(), ar.end()
+#define pq priority_queue
+inline ll Power(int b, int p) { ll ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+
+
+vector<int>::iterator j;
+
+// vector< pair < char, pair<int,int> > >v;
+int simul(int k) {
+    int d = 0;
+    for(int i = 0; i < n; i++) {
+        if(r[i] - d < k) {
+            d += r[i] - d;
+        } else if(r[i] - d == k) {
+            d += k;
+            k--;
+        } else return -1;
+    }
+    return k;
+}
+ 
+int main() {
+    scanf("%d", &T);
+    for(int i = 1; i <= T; i++) {
+        scanf("%d", &n);
+        for(int j = 0; j < n; j++)
+            scanf("%d", &r[j]);
+ 
+        int high = 11000000, low = 0;
+        while(high - low > 0) {
+            if(simul(high) >= 0) {
+                high = low + (high - low) / 2;
+            } else {
+                low = high;
+                high = high + high / 2;
+            }
+        }
+        printf("Case %d: %d\n", i, high + 1);
+    }
+ 
+    return 0;
+}
