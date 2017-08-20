9dbb3117793fe6f99ff88dead357d32a794109bf
@4vedi <4vedi.aayush@gmail.com>
2017-08-21 00:44:22 +0530

    Add files via upload

---
+++ uva11264 - Coin Collector.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+#define nl endl
+#define ll long long
+#define vb vector<bool>
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<int, string> mis;
+typedef map<string, bool> msb;
+typedef map<string, string> mss;
+typedef map<string, char> msc;
+typedef pair<int, int> pii; 
+typedef vector<pii> vii;
+#define FOR(i,a,b) for(int i=a; i<b;i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define IterV (j, v) for(vector<int>::iterator j = v.begin(); j!=v.end();j++)
+#define pb push_back
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define all(ar) ar.begin(), ar.end()
+#define pq priority_queue
+inline ll Power(int b, int p) { ll ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+
+
+vector<int>::iterator j;
+
+// vector< pair < char, pair<int,int> > >v;
+
+ 
+int main() {
+    sint T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        ll Array[N];
+        for(int i=0;i<N;i++){ cin>>Array[i];}
+        ll Biggest=Array[0];
+        int coin=1;
+        for(int i=1;i<N-1;i++){
+            if (Biggest+Array[i]<Array[i+1]){
+                Biggest+=Array[i];
+                coin++;
+            }
+        }
+        coin++;
+        cout<<coin<<endl;
+ 
+    return 0;
+}
