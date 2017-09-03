b4acce93df2bd5b831c4b924b799b3764b3a554d
@4vedi <4vedi.aayush@gmail.com>
2017-09-03 15:42:26 +0530

    Add files via upload

---
+++ longestPalindrome.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+//#include <string.h>
+using namespace std;
+
+#define nl endl
+#define ll long long
+#define vb vector<bool>
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<int, string> mis;
+typedef map<string, bool> msb;
+typedef map<string, string> mss;
+typedef map<string, char> msc;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+#define FOR(i,a,b) for(int i=a; i<b;i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define IterV (j, v) for(vector<int>::iterator j = v.begin(); j!=v.end();j++)
+#define pb push_back
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define all(ar) ar.begin(), ar.end()
+#define pq priority_queue
+inline ll Power(int b, int p) { ll ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+
+const int MAX =101;
+
+int longpalind(string &str, int n){
+   int L[n][n];
+   for (int i = 0; i < n; i++)
+      L[i][i] = 1;
+
+    for (int l=2; l<=n; l++)
+    {
+        for (int i=0; i<n-l+1; i++)
+        {
+            int j = i+l-1;
+            if (str[i] == str[j] && l == 2)
+               L[i][j] = 2;
+            else if (str[i] == str[j])
+               L[i][j] = L[i+1][j-1] + 2;
+            else
+               L[i][j] = max(L[i][j-1], L[i+1][j]);
+        }
+    }
+
+    return L[0][n-1];
+
+}
+
+int main(){
+
+    int t;cin>>t;
+    while(t--){
+        string s;cin>>s;
+        int n = s.length();
+        cout<<longpalind(s,n)<<'\n';
+
+    }
+    return 0;
+}
