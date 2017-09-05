423e9c799f3283e3abafccb672280aecde27d692
@4vedi <4vedi.aayush@gmail.com>
2017-09-05 15:03:25 +0530

    Add files via upload

---
+++ findDuplicateInO(n).cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+//#include <string.h>
+using namespace std;
+
+#define nl endl
+#define ll long long
+#define vb vector<bool>
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<int, string> mis;
+typedef map<string, bool> msb;
+typedef map<string, string> mss;
+typedef map<string, char> msc;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+#define FOR(i,a,b) for(int i=a; i<b;i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define IterV (j, v) for(vector<int>::iterator j = v.begin(); j!=v.end();j++)
+#define pb push_back
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define all(ar) ar.begin(), ar.end()
+#define pq priority_queue
+inline ll Power(int b, int p) { ll ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+
+const int MAX =101;
+
+// int noOfEncoding(char *s, int n){
+//   if(n==0 || n==1) return 1;
+//   int count =0;
+//   if(s[n-1] > '0') count = noOfEncoding(s, n-1);
+//   if(s[n-2] =='1' || (s[n-2] == '2' && s[n-1] < '7') count += noOfEncoding(s, n-2);
+//
+//   return count ;
+// }
+int main(){
+
+  int t;cin>>t;
+  while(t--){
+    int n;cin>>n;
+    int cnt=0;
+    int a[n];FOR(i,0,n) cin>>a[i];
+    FOR(i,0,n){
+      if(a[abs(a[i])] > 0){
+        a[abs(a[i])] = -a[abs(a[i])];
+      }else{
+        cnt++;
+      }
+    }
+
+    cout<<cnt<<endl;
+    }
+
+  }
+    return 0;
+}
