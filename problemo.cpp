6f2cee9c63c5138d81aa3ac974f5c545f495f8e1
a4avyeudsih <4vedi.aayush@gmail.com>
2017-10-27 12:27:44 +0530

    edge existence

--- problemo.cpp
+++ problemo.cpp
@@ -36,22 +36,108 @@ inline ll Power(int b, int p) { ll ret = 1; for ( int i = 1; i <= p; i++ ) ret *
 
 const int MAX_N = 100;
 
-int main(){
-  int t;cin>>t;
-  while(t--){
-    int n;cin>>n;
-    int a[n];FOR(i,0,n)cin>>a[i];
-    //int b[1000]={1};
-    FOR(i,0,n){
-      if(a[abs(a[i])] < 0){
-        cout<<abs(a[i])<<" ";
-      }else{
-        a[abs(a[i])] = -1;
-      }
 
+/*
+int main(){
+  int n;cin>>n;
+  vi v[n-1];
+  int g,h;
+  FOR(i,0,n-1){
+
+    cin>>g>>h;
+    v[g].pb(h);
+    v[h].pb(g);
+  }
+  int x;cin>>x;
+  int start =1, cnt=0;
+  bool marked[10000];
+  int level[10000];
+  memset(marked, false, 10000);
+  queue<int>q;
+  q.push(start);
+  marked[start]=true;
+  level[start]=1;
+  while(!q.empty()){
+    int k=q.front();
+    q.pop();
+    for(vector<int>::iterator j = v[k].begin(); j!=v[k].end();j++){
+      if(!marked[*j]){
+        marked[*j]=true;
+        level[*j]=level[k]+1;
+        if(level[*j]==x)cnt++;
+        q.push(*j);
+      }
     }
   }
-
+  cout<<cnt<<endl;
 
   return 0;
 }
+*/
+//
+// bool visited[1000];
+// vi v[1000];
+// // void initialise(){
+// //   FOR(i,0,1000)visited[i]=false;
+// // }
+// int cnt=0;
+// int dfs(int s) {
+//     visited[s] = true;
+//     cnt =0;
+//     for(int i = 0;i < v[s].size();++i)    {
+//      if(visited[v[s][i]] == false)
+//           cnt++;
+//          dfs(v[s][i]);
+//     }
+//     return cnt;
+// }
+//
+//
+// int main(){
+//   int n;cin>>n;
+//   //vi v[n-1];
+//   int a,b;
+//   FOR(i,0,n-1){
+//     cin>>a>>b;
+//     v[a].pb(b);
+//     v[b].pb(a);
+//   }
+//   int x;cin>>x;
+//   FOR(i,0,n-1){
+//     visited[i]=false;
+//   }
+//   int tmp;
+//   int ans=0;
+//   FOR(i,1,n){
+//     if(visited[i] == false){
+//       tmp=dfs(i);
+//       if(i ==x) {ans= tmp;break;}
+//     }
+//   }
+//   cout<<ans<<endl;
+// }
+vector <int> adj[10];
+
+    int main()
+    {
+        int x, y, nodes, edges;
+        cin >> nodes;       //Number of nodes
+        cin >> edges;       //Number of edges
+        for(int i = 0;i < edges;++i)
+        {
+                cin >> x >> y;
+            adj[x].push_back(y);        //Insert y in adjacency list of x
+         }
+    for(int i = 1;i <= nodes;++i)
+    {
+            cout << "Adjacency list of node " << i << ": ";
+        for(int j = 0;j < adj[i].size();++j)
+            {
+            if(j == adj[i].size() - 1)
+                    cout << adj[i][j] << endl;
+            else
+                cout << adj[i][j] << " --> ";
+    }
+    }
+    return 0;
+    }
