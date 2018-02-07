e5d194a19743f878724e00b22fcca0e729cb030f
@4vedi <4vedi.aayush@gmail.com>
2018-02-08 01:56:02 +0530

    Add files via upload

---
+++ dp/coin_change.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define nl endl
+#define lli long long int
+#define ld long double
+#define vb vector<bool>
+typedef vector<int> vi;
+typedef vector<lli> vlli;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef priority_queue<int> pq; //maxHeap
+typedef priority_queue <int, vector<int>, greater<int> > pqn; //minHEAP
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<int, string> mis;
+typedef map<string, bool> msb;
+typedef map<string, string> mss;
+typedef map<string, char> msc;
+typedef pair<int, int> pii;
+typedef pair<lli, lli> pllilli;
+typedef vector<pii> vii;
+typedef vector<pllilli> vllilli;
+//vector<int>::iterator j;
+#define REPV (j, v) for(vector<int>::iterator j = v.begin(); j!=v.end();j++)
+#define pb push_back
+//#define pf push_front
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPR(I, A, B) for (int I = (B); I > (A); I--)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRLLI(X) lli (X); scanf("%lld", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRLLILLI(X, Y) lli X, Y; scanf("%lld%lld", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define DRLLILLILLI(X, Y, Z) lli X, Y, Z; scanf("%lld%lld%lld", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define DRA(a,n) int a[n]; REP(i,n)cin>>a[i]
+#define l_b lower_bound
+#define u_b upper_bound
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define NL cout<<endl
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define all(ar) ar.begin(), ar.end()
+#define zzz return 0
+inline lli Power(int b, int p) { lli ret = 1; for ( int i = 1; i <= p; i++ ) ret *= b; return ret; }
+const int MOD = 1e9+7;
+const int SIZE = 4e6+10;
+const int MAX = 1e9+1;
+
+
+//Optimal substructure: f(a[],n,N)=f(a[],n-1,N)+f(a[],n,N-am)//(i)not taking the n'th coin and (ii)taking it
+//O(m*n)
+int coin_change(int a[],int n,int N)
+{
+  int dp[N+1][n];//extra row for base case 0
+  REP(i,0,n)dp[0][i]=1;//bcz if N==0; then only 1 way is possible-don't take any coin
+
+  //fill the table in bottom_up manner
+  REP(i,1,N+1){
+    REP(j,0,n){
+      //including a[j]
+      int x = (i-a[j] >=0)?dp[i-a[j]][j]:0;
+      //excluding a[j]
+      int y = (j>=1)?dp[i][j-1]:0;
+      dp[i][j]=x+y;
+    }
+  }
+  return dp[N][n-1];
+}
+int main(){
+  DRI(n);
+  int a[n];
+  REP(i,0,n){
+    cin>>a[i];
+  }
+  DRI(N);
+  cout<<coin_change(a,n,N);NL;
+  zzz;
+}
+//Space complexity: O(n)
+int count( int S[], int m, int n )
+{
+    // table[i] will be storing the number of solutions for
+    // value i. We need n+1 rows as the table is constructed
+    // in bottom up manner using the base case (n = 0)
+    int table[n+1];
+
+    // Initialize all table values as 0
+    memset(table, 0, sizeof(table));
+
+    // Base case (If given value is 0)
+    table[0] = 1;
+
+    // Pick all coins one by one and update the table[] values
+    // after the index greater than or equal to the value of the
+    // picked coin
+    for(int i=0; i<m; i++)
+        for(int j=S[i]; j<=n; j++)
+            table[j] += table[j-S[i]];
+
+    return table[n];
+}
